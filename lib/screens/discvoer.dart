4bef34cf36e9497e5d38bf47d72506ed198f4dbf
Nizar Sweiss <Nizarsweiss00@gmail.com>
2022-09-22 16:28:21 +0300

    Web Pages Added

--- lib/screens/discvoer.dart
+++ lib/screens/discvoer.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
-
+import 'package:a_two/models/other/WebPages';
 import 'package:a_two/models/urlModel.dart';
 import 'package:a_two/models/vehicles/bike.dart';
 import 'package:a_two/models/vehicles/car.dart';
@@ -22,6 +22,7 @@ class Discover extends StatelessWidget {
     List mercedesBenz_cars = [];
     List ferrari_cars = [];
     List porche_cars = [];
+     List webPages = [];
     carsBrand.addAll([mercedesBenz_cars, ferrari_cars, porche_cars]);
 
     void addAllCarsFor_MercedesBenz() {
@@ -74,6 +75,54 @@ class Discover extends StatelessWidget {
                 "https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/640x400/quality/80/https://s.aolcdn.com/commerce/autodata/images/USC30FRC171A021001.jpg"),
       );
     }
+   webPages.add(WebPages(
+        brand: "Mercedes-Benz",
+        pageUrl: 'https://www.mercedes-benz.com/en/',
+        imageURL:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Mercedes-Logo.svg/1200px-Mercedes-Logo.svg.png",
+        subTitle: "Join our Mercedes-Benz page"));
+
+
+         webPages.add(WebPages(
+        brand: "Ferrari",
+        pageUrl: 'https://www.ferrari.com/en-JO',
+        imageURL:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjWHkwaInDn7GbeEAsPBMKSGv8rAkoqFmwAtlLLa3mufhwOMKNMtvVuVKvGJEk3SGyTIE&usqp=CAU",
+        subTitle: "Join our Ferrari page"));
+
+
+         webPages.add(WebPages(
+        brand: "Porsche",
+        pageUrl: 'https://www.porsche.com/middle-east/_jordan_/',
+        imageURL:
+            "https://png.monster/wp-content/uploads/2022/02/png.monster-758.png",
+        subTitle: "Join our Porsche page"));
+
+
+         webPages.add(WebPages(
+        brand: "BMW",
+        pageUrl: 'https://www.bmw.com/en/index.html',
+        imageURL:
+            "https://png.monster/wp-content/uploads/2021/03/pngwing.com-4-208ee93f-370x370.png",
+        subTitle: "Join our BMW page"));
+
+  webPages.add(WebPages(
+        brand: "Harley Davidson",
+        pageUrl: 'https://www.harley-davidson.com/ac/en/index.html',
+        imageURL:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Harley-Davidson_logo.svg/2560px-Harley-Davidson_logo.svg.png",
+        subTitle: "Join our Harley Davidson page"));
+
+  var webPage = ListView.builder(
+        itemCount: webPages.length,
+        itemBuilder: (BuildContext context, int index) {
+          return  URlClass(
+            myfunction: () => goToThisPage(webPages[index].getPageUrl()),
+            carBrandsImage: webPages[index].getImageURL(),
+            myplatformSubTitle: webPages[index].getSubTitle(),
+            myplatformTitle: webPages[index].getBrand(),
+          );
+        });
 
     void addAllCarsFor_Porche() {
       porche_cars.add(
@@ -86,6 +135,8 @@ class Discover extends StatelessWidget {
                 "https://www.kindpng.com/picc/m/574-5744017_luxury-cars-for-less-porsche-cayman-white.png"),
       );
     }
+
+    
 
     addAllCarsFor_MercedesBenz();
     addAllCarsFor_Ferrari();
@@ -232,23 +283,7 @@ class Discover extends StatelessWidget {
     iconTabs.addAll({
       Icon(Ionicons.car_sport): structureVehiecles(carsBrand),
       Icon(Ionicons.bicycle): structureVehiecles(bikesBrand),
-      Icon(Ionicons.share_social): Column(
-        children: [
-          URlClass(
-            myfunction: () => goToThisPage('https://www.facebook.com'),
-            myplatformIcon: Icons.facebook,
-            myplatformSubTitle: "join our facebook page",
-            myplatformTitle: "Facebook",
-          ),
-          URlClass(
-            myfunction: () =>
-                goToThisPage('https://www.instagram.com/abcit000/'),
-            myplatformIcon: Ionicons.logo_instagram,
-            myplatformSubTitle: "join our instagram page",
-            myplatformTitle: "Instagram",
-          ),
-        ],
-      ),
+      Icon(Ionicons.share_social): webPage
     });
 
     return DefaultTabController(
